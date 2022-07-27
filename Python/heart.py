dae6d8bd93e1ddd71523449a75d251638de64786
Pythadela <107359025+Pythadela@users.noreply.github.com>
2022-07-27 16:07:27 +0200

    My very first script in Python.

---
+++ Python/heart.py
@@ -0,0 +1,26 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+for nums in range (9):
+    for num in range(6):
+        print(grid[nums][num], end=' ')
+    print()
+
+
+print()
+
+for nums in range (6):
+    for num in range(9):
+        print(grid[num][nums], end=' ')
+    print()
+
+input()
+
